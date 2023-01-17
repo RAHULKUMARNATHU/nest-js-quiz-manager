8cef677d6ea140ab295a5311d9ce16985822b96d
Rahul Kumar <rahul.kumar@brainvire.com>
2023-01-17 13:52:49 +0530

    commit :: error at applying repository

---
+++ src/module/questions/entities/question.repository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from "typeorm"
+import { Question } from "./question.entity"
+
+
+@EntityRepository(Question)
+
+export class QuestionRepository extends Repository<Question>{
+
+}
--- src/module/quiz/entities/quiz.repository.ts
+++ src/module/quiz/entities/quiz.repository.ts
@@ -1,9 +1,15 @@
-// import { EntityRepository, Repository } from "typeorm"
-// import { Quiz } from "./quiz.entity";
-
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { CreateQuizDto } from '../dto/create-quiz.dto';
+import { Quiz } from './quiz.entity';
 
 // @EntityRepository(Quiz)
+@Injectable()
+export class quizRepository extends Repository<Quiz> {
 
-// export class quizRepository extends Repository<Quiz>{
 
-// }
+    constructor (private dataSource : DataSource){
+        super(Quiz , dataSource.createEntityManager())
+}
+// sync create({title , description}) : CreateQuizDto :Promise<Quiz>{}
+}
