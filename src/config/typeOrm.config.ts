a1d8598faa2215f41877b4bb26aba54fb62594db
Rahul Kumar <rahul.kumar@brainvire.com>
2023-01-24 15:33:44 +0530

    skipping  migration Here

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -12,10 +12,37 @@ export class DatabaseConnectionService implements TypeOrmOptionsFactory {
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_DB,
-      synchronize: true,
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/**/*.migrations.js'],
+      // cli: {
+      //   migrationsDir: __dirname + '/../database/migrations',
+
+      // },
+      extra: {
+        charset: 'utf8mb4_unicode_ci',
+      },
+      synchronize: false,
       dropSchema: false,
       logging: true,
-      entities: ['dist/**/*.entity.js'],
     };
   }
 }
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  entities: ['dist/**/*.entity.js'],
+  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+  // cli: {
+  //   migrationsDir: __dirname + '/../database/migrations',
+  // },
+  extra: {
+    charset: 'utf8mb4_unicode_ci',
+  },
+  synchronize: false,
+  logging: true,
+};
