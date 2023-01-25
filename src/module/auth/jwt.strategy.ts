de48785461207dcc87ebec6fab95be6ac9f8faf1
Rahul Kumar <rahul.kumar@brainvire.com>
2023-01-25 19:01:58 +0530

    commit :: jwt config

--- src/module/auth/jwt.strategy.ts
+++ src/module/auth/jwt.strategy.ts
@@ -1,11 +1,12 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import appConfig from 'src/config/app.config';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'dgshdjseds',
+      secretOrKey: appConfig().appSecret,
     });
   }
 
