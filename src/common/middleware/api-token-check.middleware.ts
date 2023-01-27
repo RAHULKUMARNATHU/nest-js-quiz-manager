35668674546fca97c39f037a247ce7d06e92674d
Rahul Kumar <rahul.kumar@brainvire.com>
2023-01-27 15:21:33 +0530

    commit :: custom exception

--- src/common/middleware/api-token-check.middleware.ts
+++ src/common/middleware/api-token-check.middleware.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, NestMiddleware } from "@nestjs/common";
+import { BadRequestException, HttpException, HttpStatus, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
+import { ApiTokenPaymentException } from "../exceptions/api-token-payment.exception";
 
 
 
@@ -7,7 +8,10 @@ export class ApiTokenCheckMiddleware implements NestMiddleware{
     use(req: Request, res: Response, next: NextFunction) {
         // console.log(req.headers);
         if(req.headers['api-token'] != 'my-token'){
-            throw new BadRequestException('The token does not match')
+            // throw new BadRequestException('The token does not match')
+            // throw new HttpException('My Response', HttpStatus.PAYMENT_REQUIRED)
+            
+            throw new ApiTokenPaymentException
         }
         
         next();
